8bfe186cf2e3b9050514e30d941b1d0ca046d3cc
apoorva srivastava <apoorvasrivastava.14@gmail.com>
2021-05-23 14:42:11 +0530

    added landing page

--- Web Development/ambher/src/App.js
+++ Web Development/ambher/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg'
 import './App.css'
+import Landing from './pages/Landing'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello Ambher!</h1>
+        <Landing />
       </header>
     </div>
   )
---
+++ Web Development/ambher/src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>This is landing page. render your components here.</h1>
+    </div>
+  )
+}
+
+export default Landing
