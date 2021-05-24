e80a7a1ad4dd7a15f641025d20637e5824a7dbcc
Yash Ray <ray.yash.08@gmail.com>
2021-05-24 13:45:44 +0530

    Header and search ready

--- Web Development/ambher/src/components/hamburger/hamburger.component.jsx
+++ Web Development/ambher/src/components/hamburger/hamburger.component.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import "./hamburger.style.css";
+import { Sidebar } from "../sidebar/sidebar.component";
 
 export const Hamburger = () => (
-  <div className="outer-ham">
-    <div className="inner-ham" onClick={() => {
-        console.log(Math.random());
-        document.getElementById()
-    }}>
+  <div
+    className="outer-ham"
+    onClick={() => {
+      console.log(Math.random());
+      document.getElementById("1").classList.toggle("rot-down");
+      document.getElementById("3").classList.toggle("rot-up");
+      document.getElementById("2").classList.toggle("slide");
+
+      document
+        .getElementById("sidebar-control")
+        .classList.toggle("sidebar-slide");
+    }}
+  >
+    <div className="inner-ham">
       <div className="menu" id="1"></div>
       <div className="menu" id="2"></div>
       <div className="menu" id="3"></div>
+    </div>
+    <div id="sidebar-control">
+      <Sidebar />
     </div>
   </div>
 );
