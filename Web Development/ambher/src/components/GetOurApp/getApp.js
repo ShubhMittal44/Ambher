d01ad07608dfbf6c70f51af7258aa3c95bc1e03e
5CITe <arch.hack28@gmail.com>
2021-05-26 19:10:07 +0530

    Get Our app section and Images folder changed

---
+++ Web Development/ambher/src/components/GetOurApp/getApp.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../GetOurApp/getApp.css'
+import Phone from '../../assets/images/Mockup-crop.png'
+import Astore from '../../assets/images/applestorebutton.png'
+import Pstore from '../../assets/images/playstorebutton.png'
+
+const GetOurApp = () => {
+  return (
+    <div className="container-fluid"  >
+      <div class="row">
+      <div class="col-sm-7">
+        <h1>Get Our App</h1>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam</p>
+        <br></br>
+        <img src={Astore}></img>
+        <img src={Pstore}></img>
+      </div>
+      
+      <div class="col-sm-1">
+        <img src={Phone} class="left"></img>
+      </div>
+      <div class="col-sm-3">
+        <img src={Phone} class="middle"></img>
+      </div>
+      <div class="col-sm-1" >
+        <img src={Phone} class="right"></img>
+      </div>
+      </div> 
+    </div>
+  )
+}
+
+export default GetOurApp;
--- Web Development/ambher/src/pages/Landing.js
+++ Web Development/ambher/src/pages/Landing.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import GetOurApp from '../components/GetOurApp/getApp.js'
 
 const Landing = () => {
   return (
     <div>
       <h1>This is landing page. render your components here.</h1>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <h2>hello</h2>
+      <GetOurApp />
+      <h2>hello</h2>
     </div>
   )
 }
