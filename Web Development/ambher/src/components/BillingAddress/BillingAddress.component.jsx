cf490b9bff0a9d38db49a6d5827127aa20029c03
Yash Ray <ray.yash.08@gmail.com>
2021-07-17 01:34:09 +0530

    Billing Address component added but is not responsive

--- Web Development/ambher/src/components/BillingAddress/BillingAddress.component.jsx
+++ Web Development/ambher/src/components/BillingAddress/BillingAddress.component.jsx
@@ -1,10 +1,40 @@
 import React from "react";
-import './BillingAddress.style.css';
+import EditIcon from "@material-ui/icons/Edit";
+import WarningIcon from "@material-ui/icons/Warning";
+import "./BillingAddress.style.css";
 
 export const BillingAddress = (props) => {
   return (
-      <div className="bill-address">
-          {/* Dummy */}
+    <div className="bill-address">
+      <div id="bill-top">
+        <div id="bill-heading">Billing Address</div>
+        <div id="edit">
+          <EditIcon />
+        </div>
       </div>
+      <div id="bill-bottom">
+        <div id="bill-scp">
+          <div className="scp-itm">
+            <div className="scp-sub-head">State</div>
+            <div className="scp-sub-ans">Karnataka</div>
+          </div>
+          <div className="scp-itm">
+            <div className="scp-sub-head">City</div>
+            <div className="scp-sub-ans">Bangalore</div>
+          </div>
+          <div className="scp-itm">
+            <div className="scp-sub-head">Pincode</div>
+            <div className="scp-sub-ans">560037</div>
+          </div>
+        </div>
+        <div id="bill-add">
+          <div className="scp-sub-head">Address</div>
+          <div className="scp-sub-ans">
+            Chourasia Hieght", #22, Ground Floor, First Cross, Ashwathnagar,
+            Marathalli, Bangalore, Pin Code - 560037, Karnataka, INDIA
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
